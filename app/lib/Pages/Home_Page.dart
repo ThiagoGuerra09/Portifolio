293c9edbcb3ee4c48a16a41265319436e6b5901d
ThiagoGuerra09 <thiagogwf@gmail.com>
2022-11-08 14:23:48 -0300

    AppBarPronta

--- app/lib/Pages/Home_Page.dart
+++ app/lib/Pages/Home_Page.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -16,13 +18,53 @@ class _HomePageItemState extends State<HomeScreen> {
         appBar: PreferredSize(
       preferredSize: Size.fromHeight(50),
       child: Container(
+        height: 50,
         decoration: const BoxDecoration(
           gradient: LinearGradient(
             colors: <Color>[
               Color.fromARGB(255, 22, 25, 28),
-              Color.fromARGB(255, 94, 86, 89)
+              Color.fromARGB(255, 107, 104, 105)
             ],
           ),
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 18.0),
+                    child: Column(
+                      textDirection: TextDirection.ltr,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Thiago Guerra"),
+                        Text(
+                          "Desenvolvedor Mobile",
+                          style: TextStyle(color: Colors.white, fontSize: 9.0),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text("Sobre mim"),
+                  Text("Experiências"),
+                  Text("Tecnologias"),
+                  Text("Contatos")
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     ));
